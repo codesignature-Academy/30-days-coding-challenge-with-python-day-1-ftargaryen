44384a48aaba4066d219e239dc29b7d4436b7014
santino <franklinifeanyi16@gmail.com>
2025-11-13 18:40:14 +0100

    initial commit

---
+++ day1/assignment1.py
@@ -0,0 +1,5 @@
+name = input("Enter your name: ")
+hobby = input("Enter your favourite hobby: ")
+hob_duration = input("How many years have you done it: ")
+
+print(f"Hi, my name is {name}. I have been {hobby} for {hob_duration}. And I love it!")
